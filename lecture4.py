c4983f03eb4d8faad5f9760cf6d6a2259a897371
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-08-24 22:42:21 +0300

    Add files via upload

---
+++ lecture4.py
@@ -0,0 +1,35 @@
+Nint = 12
+Nfloat = 32.112
+Ncomplex = 77j
+
+#convert int to float :
+a=float(Nint)
+
+#convert int to complex :
+b=complex(Nint)
+
+#convert float to int :
+c=int(Nfloat)
+
+#convert float to complex :
+d=complex(Nfloat)
+
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+import random
+print(random.randrange(100,200))
+print(random.randrange(10,100))
+print(random.randrange(0,10))
