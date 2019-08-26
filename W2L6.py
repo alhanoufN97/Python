804a9e94964c67ae5d6fd285979694ca0061ea58
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-08-26 17:04:23 +0300

    Add files via upload

---
+++ W2L6.py
@@ -0,0 +1,18 @@
+a=int("98")
+b=int(4.6)
+
+c=float("77.18")
+d=float(1108)
+
+e=str(889)
+f=str("AL")
+print("\n****INT****")
+print(type(a) , a )
+print(type(b) , b )
+print("\n****FLOAT****")
+print(type(c) , c )
+print(type(d) , d )
+print("\n****STRING****")
+print(type(e) , e )
+print(type(f) , f )
+    
