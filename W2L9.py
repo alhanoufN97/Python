96894e247f050d8601dba4c1fd0286f03779537c
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-08-28 23:40:19 +0300

    Add files via upload

---
+++ W2L9.py
@@ -0,0 +1,9 @@
+L1="java"
+L2="c++"
+L3="python"
+
+text="I can write code in {} , Now i learn how to write code in {} , I hope learning {} language "
+
+print(text.format(L1,L3,L2)+"\n\n")
+
+print("I can write code in {0} , Now i learn how to write code in {2} , I hope learning {1} language ".format(L1,L2,L3))
