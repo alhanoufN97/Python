3db3c3f12c11e2e9f9821086603afdb614cdfeda
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-08-30 23:22:51 +0300

    Add files via upload

---
+++ W2L11.py
@@ -0,0 +1,9 @@
+N1=["Nora" , "sara" ,"Ali","Ahmed"]
+N2 =["Nora" , "sara" ,"Ali","Ahmed"]
+N3=N2
+
+print(N1 is N2)
+print(N3 is N2)
+print("Ali" in N2 and "Ali" in N1 and "Alhanouf" in N3 )
+print("sara" in N2 or "Alhanouf" in N1)
+
