5ee3b1de74912527b8100f78cce1650c700faced
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-01 01:50:13 +0300

    Add files via upload

---
+++ W2L12.py
@@ -0,0 +1,7 @@
+x="￼ Please, I want {} sandwishes and {} donutes "
+print("----Q1----")
+print(x.replace('I','We'))
+print("----Q2----")
+print(x.format('5','7'))
+print("----Q3----")
+print(x.replace('a','A'))
