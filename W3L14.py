eef05291da6a7b7136d2f26531bfe3243b56d045
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-03 02:52:30 +0300

    Add files via upload

---
+++ W3L14.py
@@ -0,0 +1,14 @@
+List1 =[1 , 2 , 3 , 4 , 5 ]
+List2 =['A' , 'B' , 'C' , 'D' ]
+
+print(List1[2:4])
+
+print(List2[2]*3)
+
+print(List1+List2)
+
+if 'x' in List2 :
+    print("IT IS HERE !")
+else :
+    print(List2)
+    
