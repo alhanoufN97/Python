39504a1c290e9fa343bafc4b9783c71644762843
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-04 01:04:17 +0300

    Add files via upload

---
+++ W3L16.py
@@ -0,0 +1,13 @@
+table=(77, "ABC" , 22 , 98.11 , "python" , 34 )
+print(table)
+print(table[2:5])
+
+for x in table :
+    print(x)
+
+print("\n--- integer type ---")
+for x in table :
+    if(type(x)==int):
+        print(x)
+    
+    
