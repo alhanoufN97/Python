2e4bd0a2c288816b997bcb8749b30c2076662c5b
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-07 05:34:14 +0300

    Add files via upload

---
+++ W3L17.py
@@ -0,0 +1,12 @@
+tab1=tuple(("A","B","C","D"))
+print(tab1)
+tab2=("1","2","3","4","5")
+tab=(" python_course ",)*5
+print(tab)
+tab1+=tab2
+print(tab1)
+print(len(tab1))
+if 'A'in tab1 :
+    print("True")
+else :
+    print("False")
