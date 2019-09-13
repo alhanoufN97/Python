30eb575b6727d46074e1ec829d4aad6394d4cbd8
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-13 16:49:27 +0300

    Add files via upload

---
+++ W4L21.py
@@ -0,0 +1,15 @@
+theset = {"A","B","C",1,2}
+print(len(theset))
+
+theset.remove(1)
+print(theset)
+theset.discard(2)
+print(theset)
+theset.pop()
+print(theset)
+theset.clear()
+print(theset)
+theset = set(("1","2","3","4","5"))
+print(theset)
+del theset
+print(theset)
