8879298ed797beae6e8a60d01ff019a3507a3f05
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-16 04:02:57 +0300

    Add files via upload

---
+++ W4L25&26.py
@@ -0,0 +1,20 @@
+print("-----Q1----")
+set1={1,3,5,7,8}
+
+set1.update([4,8,12])
+print(set1)
+
+set1.remove(8)
+print(set1)
+
+print("-----Q2----")
+
+T={
+    "name":"pigeon" ,
+    "type":"bird" ,
+    "skin cover":"feathers"
+    }
+print("the type   : "+ T["type"])
+
+T["skin cover"]="---"
+print(T)
