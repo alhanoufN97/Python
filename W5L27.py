d09e66e5cb16c1a23cdcc871334d4ccc91c74da1
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-16 04:20:25 +0300

    Add files via upload

---
+++ W5L27.py
@@ -0,0 +1,31 @@
+x , b , z=15 , 6 ,15
+
+if(x==b) :
+    print("equal")
+elif (x>b) :
+    print("x greater than b ")
+else :
+    print("x less than b ")
+
+print(z) if(x<z) else print(x)if(x>z) else print("=")
+
+if x>b and z<b :
+    print ("true")
+else:
+    print("false")
+
+
+if x>b or z<b :
+    print ("true")
+else:
+    print("false")
+
+
+if x!=b :
+    print("not equal!")
+    if x<b :
+            print("x<b")
+    else :
+        print("x>b")
+          
+
