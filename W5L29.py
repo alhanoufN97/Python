b1732887ff1fb21bd48609e96f3ca3d21904eaa4
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-17 01:08:04 +0300

    Add files via upload

---
+++ W5L29.py
@@ -0,0 +1,13 @@
+T = ["AA" , "AK" ,"AS" ,"AC","AG" ,"AH", "AF"]
+
+for x in T :
+    if x=="AS" :
+        continue
+     elif x=="AH" :
+         print("end fpr loop")
+         break
+        
+    print(x)
+
+    
+    
