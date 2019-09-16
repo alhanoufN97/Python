21537a756e46b9003db32ddd60bb979f7f95e1f8
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-17 00:47:59 +0300

    Add files via upload

---
+++ W5L28.py
@@ -0,0 +1,19 @@
+print("while loop #1 : ")
+i=5
+while i>0 :
+    print (i)
+    i-=1
+
+print("while loop #2 : ")
+x=0
+while x<40 :
+    x+=1
+    if(x==10) :
+        continue
+    elif x==15:
+        break
+    print(x)
+    
+    
+     
+    
