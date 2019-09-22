2cea1fc0be1c295a6913961cfa12a0b90ef4fbbf
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-22 23:18:08 +0300

    Add files via upload

---
+++ W5L30.py
@@ -0,0 +1,27 @@
+print("Range #1 : ")
+for x in range(9) :
+    print(x)
+
+print("Range #2 : ")
+for x in range(3,9) :
+    print(x)
+
+print("Range #3 : ")
+for x in range(3,20 , 15) :
+    print(x)
+
+
+
+print("Range #4 : ")
+for x in range(3) :
+    print(x)
+else :
+    print("Final loop ")
+
+
+Digital = ["A","B","C" , "D"]
+Num = [1,2,3,4,5]
+
+for x in Digital :
+    for y in Num :
+        print(x,y)
