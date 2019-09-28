6bbe1ba41d862fb37d065b92f7a6b66f215ec640
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-09-28 19:29:58 +0300

    Add files via upload

---
+++ W6L38.py
@@ -0,0 +1,19 @@
+arr = [" Python" , "java" , "C++"]
+
+for x in arr :
+    print(x)
+
+arr.append("Swift")
+print(arr)
+
+arr.pop(2)
+print(arr)
+
+arr.remove("java")
+print(arr)
+
+arr.extend(["Java","C++","R"])
+print(arr)
+
+arr.sort()
+print(arr)
