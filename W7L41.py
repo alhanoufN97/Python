3a25dec1dbb9799d23a7837396e251daa309a6d8
alhanouf <39780603+alhanoufN97@users.noreply.github.com>
2019-10-06 17:53:08 +0300

    Add files via upload

---
+++ W7L41.py
@@ -0,0 +1,11 @@
+class PA:
+    def __init__(Obj , name , birthday ) :
+        Obj.name = name
+        Obj.birthday=birthday
+
+    def Fun(Object) :
+        print ("your name is : "+Object.name+" And your birthday is : "+Object.birthday)
+
+
+
+it1=PA("alhanouf","4/4/97").Fun() 
